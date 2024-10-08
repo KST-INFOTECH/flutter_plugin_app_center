5fb42928827199059c301640a860af3896f0a52d
VJ18032000 <vijaykumard2000@gmail.com>
2024-10-08 12:08:00 +0530

    analyiss

--- src/app_center_analytics/lib/app_center_analytics.dart
+++ src/app_center_analytics/lib/app_center_analytics.dart
@@ -1,3 +1,32 @@
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart' show defaultTargetPlatform;
+import 'package:flutter/foundation.dart' show TargetPlatform;
+
+class AppCenterAnalytics {
+  static String get id => (defaultTargetPlatform == TargetPlatform.iOS)
+      ? "MSAnalytics"
+      : "com.microsoft.appcenter.analytics.Analytics";
+
+  static const MethodChannel _channel = const MethodChannel(
+      'aloisdeniel.github.com/flutter_plugin_appcenter/appcenter_analytics');
+
+  static Future<bool> get isEnabled =>
+      _channel.invokeMethod('isEnabled').then( => r as bool);
+
+  static Future<void> setEnabled(bool isEnabled) =>
+      _channel.invokeMethod('setEnabled', <String, bool>{
+        'isEnabled': isEnabled,
+      });
+
+  static Future<void> trackEvent(String name, [Map<String, String>? properties]) =>
+      _channel.invokeMethod('trackEvent', <String, dynamic>{
+        'name': name,
+        'properties': properties ?? <String, String>{},
+      });
+}
+
+
 // import 'dart:async';
 
 // import 'package:flutter/services.dart';
@@ -26,30 +55,3 @@
 //         'properties': properties ?? <String, String>{},
 //       });
 // }
-import 'dart:async';
-import 'package:flutter/services.dart';
-import 'package:flutter/foundation.dart' show defaultTargetPlatform;
-import 'package:flutter/foundation.dart' show TargetPlatform;
-
-class AppCenterAnalytics {
-  static String get id => (defaultTargetPlatform == TargetPlatform.iOS)
-      ? "MSAnalytics"
-      : "com.microsoft.appcenter.analytics.Analytics";
-
-  static const MethodChannel _channel = const MethodChannel(
-      'aloisdeniel.github.com/flutter_plugin_appcenter/appcenter_analytics');
-
-  static Future<bool> get isEnabled =>
-      _channel.invokeMethod('isEnabled').then( => r as bool);
-
-  static Future setEnabled(bool isEnabled) =>
-      _channel.invokeMethod('setEnabled', <String, bool>{
-        'isEnabled': isEnabled,
-      });
-
-  static Future trackEvent(String name, [Map<String, String>? properties]) =>
-      _channel.invokeMethod('trackEvent', <String, dynamic>{
-        'name': name,
-        'properties': properties ?? <String, String>{},
-      });
-}
